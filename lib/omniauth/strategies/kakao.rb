41e4fa069d3306698fc5fb0471a1801e0a7f9eb8
egls0401 <egls0401@whitebrew.com>
2019-06-19 17:13:02 +0900

    kakao api version change

--- lib/omniauth/strategies/kakao.rb
+++ lib/omniauth/strategies/kakao.rb
@@ -4,7 +4,6 @@ module OmniAuth
   module Strategies
     class Kakao < OmniAuth::Strategies::OAuth2
       DEFAULT_REDIRECT_PATH = "/oauth"
-
       option :name, 'kakao'
 
       option :client_options, {
@@ -12,13 +11,14 @@ module OmniAuth
         :authorize_path => '/oauth/authorize',
         :token_url => '/oauth/token',
       }
-      
+
       uid { raw_info['id'].to_s }
 
       info do
         {
           'name' => raw_properties['nickname'],
           'image' => raw_properties['thumbnail_image'],
+          'email' => raw_kakao_account['email'],
         }
       end
 
@@ -37,7 +37,7 @@ module OmniAuth
         options[:callback_path] = previous_callback_path
         super
       end
-      
+
       # callback_uri와 관련해서 redirect_uri_mismatch 문제가 나오던것을 path match를 통해서 해결합니다.
       # 해당 문제는 https://devtalk.kakao.com/t/rest-api-omniauth/19207 에서 나오는 문제를 해결합니다.
       # NOTE If we're using code from the signed request then FB sets the redirect_uri to '' during the authorize
@@ -51,7 +51,7 @@ module OmniAuth
           options[:callback_url] || (full_host + script_name + callback_path)
         end
       end
-       
+
       def mock_call!(*)
         options.delete(:callback_path)
         super
@@ -59,11 +59,15 @@ module OmniAuth
 
     private
       def raw_info
-        @raw_info ||= access_token.get('https://kapi.kakao.com/v1/user/me', {}).parsed || {}
+        @raw_info ||= access_token.get('https://kapi.kakao.com/v2/user/me', {}).parsed || {}
       end
 
       def raw_properties
         @raw_properties ||= raw_info['properties']
+      end
+
+      def raw_kakao_account
+        @raw_kakao_account ||= raw_info['kakao_account']
       end
     end
   end
